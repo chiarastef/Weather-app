2373f86f7bcf1c5b3565e8a4739fdf9e6856d18a
Chiara <77229240+chiarastef@users.noreply.github.com>
2021-11-06 19:26:38 +0100

    Changed the city searched when launched

--- src/script.js
+++ src/script.js
@@ -151,4 +151,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
-searchCity("Morayfield");
+searchCity("Rome");
